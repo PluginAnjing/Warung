2ade667ed25b7b7024a5b69126223c5147d5749a
PluginAnjing <107978775+PluginAnjing@users.noreply.github.com>
2022-09-21 13:06:06 +0700

    Add files via upload

---
+++ warung.py
@@ -0,0 +1,25 @@
+print("Warung\n")
+
+print("Esteh")
+harga = 2500
+print("harga : ", harga)
+
+jumlah = int(input("jumlah : "))
+total = (harga * jumlah)
+print("total : ", total)
+
+bayar = int(input("bayar : "))
+kembali = bayar - total
+print("Kembalian : ",kembali )
+
+print("Bakso")
+harga = 8500
+print("harga : ", harga)
+
+jumlah = int(input("jumlah : "))
+total = (harga * jumlah)
+print("total : ", total)
+
+bayar = int(input("bayar : "))
+kembali = bayar - total
+print("Kembalian : ",kembali )
